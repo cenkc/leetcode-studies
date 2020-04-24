44e5bad9d1b65befe3809b0b7cc4bef4b62d41eb
Cenk Canarslan <cenk.canarslan@gmail.com>
2020-04-24 18:20:09 +0300

    initial commit

---
+++ src/test/java/com/cenkc/leetcode/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenkc.leetcode.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
