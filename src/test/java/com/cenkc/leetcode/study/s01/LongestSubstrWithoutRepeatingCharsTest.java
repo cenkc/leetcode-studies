b3e02f055ef23fd405435777983e987908de72ff
Cenk Canarslan <cenk.canarslan@gmail.com>
2020-04-25 16:33:31 +0300

    #3 found longest substr without repeating characters

---
+++ src/test/java/com/cenkc/leetcode/study/s01/LongestSubstrWithoutRepeatingCharsTest.java
@@ -0,0 +1,20 @@
+package com.cenkc.leetcode.study.s01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * created by cenkc on 4/25/2020
+ */
+class LongestSubstrWithoutRepeatingCharsTest {
+
+    @Test
+    public void longestSubstrWithoutRepeatingChars_shouldPass() {
+        LongestSubstrWithoutRepeatingChars longestSubstrWithoutRepeatingChars
+                = new LongestSubstrWithoutRepeatingChars();
+        assertEquals(3, longestSubstrWithoutRepeatingChars.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(1, longestSubstrWithoutRepeatingChars.lengthOfLongestSubstring("bbbbb"));
+        assertEquals(3, longestSubstrWithoutRepeatingChars.lengthOfLongestSubstring("pwwkew"));
+    }
+}
